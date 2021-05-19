8814a06958a6fc1bb502bf4126b4452b0a88a0dc
Nic Babineaux <76629649+NicBab@users.noreply.github.com>
2021-05-19 17:16:19 -0500

    deckbuilder part 1

---
+++ src/components/DeckList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DeckList = (props) => {
+    return (
+        <>
+          <h3>Your deck so far:</h3>
+        </>
+    );
+};
+
+export default DeckList;
---
+++ src/components/SearchResults.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SearchResults = ({results}) => {
+    return (
+        <>
+          <h3>Here's what we found ({results.length} results):</h3>
+        </>
+    );
+};
+
+export default SearchResults;
